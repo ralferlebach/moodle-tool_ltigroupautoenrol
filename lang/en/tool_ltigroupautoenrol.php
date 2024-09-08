04264c3a4a7bfaf18c5b8c33381b18717eeb1bda
ralferlebach <ralf.erlebach@gmx.de>
2024-09-08 19:43:28 +0200

    Code Check fixed

--- lang/en/tool_ltigroupautoenrol.php
+++ lang/en/tool_ltigroupautoenrol.php
@@ -26,12 +26,12 @@
 defined('MOODLE_INTERNAL') || die;
 
 $string['auto_group_enrol_form_no_group_found'] = 'Create groups first !';
-$string['auto_group_form_page_title'] = 'LTI-enrol settings';
 $string['auto_group_form_enable_enrol'] = 'Enable automatic enrolment in groups for this course';
-$string['auto_group_form_rand_method'] = 'Random enrolment';
-$string['auto_group_form_usegroupslist'] = 'Enrol users in selected groups only :';
 $string['auto_group_form_groupslist'] = 'Choose groups :';
+$string['auto_group_form_page_title'] = 'LTI-enrol settings';
+$string['auto_group_form_rand_method'] = 'Random enrolment';
 $string['auto_group_form_rolelist'] = 'Select role(s) concerned';
+$string['auto_group_form_usegroupslist'] = 'Enrol users in selected groups only :';
 $string['coursemenu_item'] = 'LTI-enrol in groups bla bla';
 $string['menu_auto_groups'] = 'LTI-enrol in groups bli blubb';
 $string['pluginname'] = 'Automatic enrolment in groups by LTI deployment';
