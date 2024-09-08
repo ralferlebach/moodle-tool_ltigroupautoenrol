9cc1bed825f42ac8e3b2f1886cbc33951bacf33b
ralferlebach <ralf.erlebach@gmx.de>
2024-09-08 19:29:16 +0200

    Fix CodeCheck

--- lang/en/tool_ltigroupautoenrol.php
+++ lang/en/tool_ltigroupautoenrol.php
@@ -25,18 +25,14 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$string['pluginname'] = 'Automatic enrolment in groups by LTI deployment';
-$string['coursemenu_item'] = 'LTI-enrol in groups bla bla';
-
-// Buttons and links.
-$string['menu_auto_groups'] = 'LTI-enrol in groups bli blubb';
-
-// Course settings form.
+$string['auto_group_enrol_form_no_group_found'] = 'Create groups first !';
 $string['auto_group_form_page_title'] = 'LTI-enrol settings';
 $string['auto_group_form_enable_enrol'] = 'Enable automatic enrolment in groups for this course';
 $string['auto_group_form_rand_method'] = 'Random enrolment';
 $string['auto_group_form_usegroupslist'] = 'Enrol users in selected groups only :';
 $string['auto_group_form_groupslist'] = 'Choose groups :';
 $string['auto_group_form_rolelist'] = 'Select role(s) concerned';
-$string['auto_group_enrol_form_no_group_found'] = 'Create groups first !';
+$string['coursemenu_item'] = 'LTI-enrol in groups bla bla';
+$string['menu_auto_groups'] = 'LTI-enrol in groups bli blubb';
+$string['pluginname'] = 'Automatic enrolment in groups by LTI deployment';
 $string['privacy:null_reason'] = 'No userdata collected by this plugin.';
